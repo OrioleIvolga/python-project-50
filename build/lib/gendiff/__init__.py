d7d7e8e2bf2a7588df1a3a9583eeeb5008ff508f
OrioleIvolga <kluchnikova@mail.ru>
2026-01-21 22:31:31 +0300

    chore: remove build artifacts and cache from repo

--- build/lib/gendiff/__init__.py
+++
@@ -1,5 +0,0 @@
-# gendiff/__init__.py
-
-def generate_diff(filepath1, filepath2, format_name='stylish'):
-    # Временная заглушка — позже вы замените её на настоящую логику
-    return f"Diff between {filepath1} and {filepath2} in format '{format_name}'"
